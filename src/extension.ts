f9b57fa70eb7f97afe47fdb058c9c32bb96188bd
Ryan Reed <Ryan.Reed@usengineering.com>
2021-01-05 22:39:53 -0600

    TODO: Comments

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,13 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) =>
     vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`)
   );
+
+  /* 
+	TODO: Rename all of the functions from nodeDependencies to the current issues list
+	TODO: Build the first list of issues by name that match the current filter. 
+	TODO: Add current you track card to the bottom ribbon. 
+	*/
+
   vscode.commands.registerCommand('currentIssues.configureSettings', () =>
     vscode.commands.executeCommand(`workbench.action.openSettings`, `youtrack`)
   );
