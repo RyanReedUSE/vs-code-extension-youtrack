beed1cf8ae7ae9b97f7ecb7df83d14fb542382f5
Ryan Reed <Ryan.Reed@usengineering.com>
2021-01-10 16:34:27 -0600

    Update: css Fixes

--- src/view/app/issue.tsx
+++ src/view/app/issue.tsx
@@ -32,17 +32,14 @@ export default class IssuePreview extends React.Component<IIssueProps> {
     return (
       !!this.props.issueData && (
         <>
-          <div>
-            <h1 className="">{this.props.issueData.idReadable}</h1>
-          </div>
           <div className="grid">
-            <h1 className="text-3xl text-bold">{this.props.issueData.idReadable}</h1>
-            <h1 className="text-3xl">{this.props.issueData.summary}</h1>
+            <h1 className="">
+              <b>{this.props.issueData.idReadable}</b> {this.props.issueData.summary}
+            </h1>
           </div>
-          <hr></hr>
+          <hr className="mt-4 mb-4"></hr>
           <div className="grid grid-cols-3">
             <div className="col-span-2">
-              <h3>Issue Description</h3>
               <ReactMarkdown plugins={[gfm]} children={this.props.issueData.description}></ReactMarkdown>
             </div>
             <div className="col-span-1">Issue Custom Fields</div>
